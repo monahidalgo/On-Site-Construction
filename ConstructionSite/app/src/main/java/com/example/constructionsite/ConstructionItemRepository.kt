c023d64d7f454be6175c2129139742ceb0532aed
Mona Hidalgo <monaego@live.com>
2024-10-18 07:00:05 -0600

    Removed second app from files.

--- ConstructionSite/app/src/main/java/com/example/constructionsite/ConstructionItemRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.example.constructionsite
-
-class ConstructionItemRepository(private val database: ConstructionDatabase) {
-    fun getAllItems(): List<ConstructionItem> {
-        return database.constructionItemDao().getAllItems()
-    }
-}
--- ConstructionSite/app/src/main/java/com/example/constructionsite/DashboardScreen.java
+++
@@ -1,6 +0,0 @@
-package com.example.constructionsite;
-
-import android.app.Activity;
-
-public class DashboardScreen extends Activity {
-}
